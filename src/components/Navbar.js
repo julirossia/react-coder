e669ce3baae924af399b9f0f07686bd08e5df47d
juli <julirossia@gmail.com>
2020-10-21 09:06:00 -0300

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../App.scss";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="menu">
+        
+          <h3 className="logo">
+            ecommerce-app
+          </h3>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
